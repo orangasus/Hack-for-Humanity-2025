0f1ac0c8616a7f5e8f4a135aaf05bd9ad91763b3
Styro <69657780+Styro457@users.noreply.github.com>
2025-01-31 17:20:51 +0200

    Modify get_username url

--- my_project/users/urls.py
+++ my_project/users/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     # URL pattern for getting a session variable
     path('get_session/', get_session, name='get_session'),
      # URL pattern for getting a session variable
-    path('get_session/', get_session_username, name='get_session'),
+    path('get_username/', get_session_username, name='get_session'),
     # URL pattern for deleting a session variable
     path('delete_session/', delete_session, name='delete_session'),
 
